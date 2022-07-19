2746478030ea4bc3cc61fe69009b129bd12bd353
Lucio <78000598+lucio-crDEV@users.noreply.github.com>
2022-07-19 00:57:00 -0400

    Add files via upload

---
+++ sh3.sh
@@ -0,0 +1,41 @@
+
+#Actualizando dependencias
+sudo apt update -y && sudo apt upgrade -y;
+
+#instalando nuevas dependencias
+sudo apt install cowsay;
+sudo apt install figlet;
+sudo apt install lolcat; 
+sudo apt install git;
+sudo apt install neo-vim;
+sudo apt install zsh; 
+sudo apt purge nodejs; 
+
+#solicitando version node e isntalandola
+echo "Ingrese la version de node que dese instalar\n"
+echo "\tversion: ";  read arg 
+
+curl -sL https://deb.nodesource.com/setup_$arg.x | sudo -E bash -
+sudo apt install nodejs
+
+#configurando GIT 
+git config --global init.defaultBranch main;
+
+
+echo -e "Ingrese credenciales GIT: email\n"
+read email
+echo -e "Ingrese su usuario\n"
+read user 
+
+git config --global user.email "$email"
+git config --global user.name "$user"
+
+
+#Setea zsh como bash predeterminada
+chsh -s $(which zsh)
+
+#mensaje final
+figlet "Toodo listo" |	lolcat 
+
+
+
