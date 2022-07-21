c6fdb519281358d009d1b99caef841a2c0dffd68
Lucio <78000598+lucio-crDEV@users.noreply.github.com>
2022-07-20 21:34:54 -0400

    Create sh4.sh

---
+++ sh4.sh
@@ -0,0 +1,9 @@
+#actualizando dependencias
+sudo apt update -y && sudo apt upgrade -y
+
+#agregando dependencias
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+zsh
