c491408c17c7eb0d37a59c8960ae0ef4b3ae3bfa
lucio-crDEV <lucio.m.cardenas@gmail.com>
2022-07-27 20:43:14 -0400

    mejorando automatizacion

--- sh4.sh
+++ sh4.sh
@@ -8,4 +8,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
+source ~./zshrc
+
 zsh
+
+p10k config
+
