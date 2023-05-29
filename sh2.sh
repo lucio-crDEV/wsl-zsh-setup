1b7f635d642dc4517231fbcc055a983a33dfb915
Lucio <78000598+lucio-crDEV@users.noreply.github.com>
2023-05-29 07:03:31 -0400

    Update sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -9,8 +9,12 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-# Carga la configuración de Zsh sin iniciar una nueva sesión
-source ~/.zshrc
+# Configuración para cerrar la terminal al salir
+if [ "$SHLVL" = 1 ]; then
+    trap "exit" INT TERM
+    export TERM=linux
+    kill -s TERM $$
+fi
 
 # Reiniciar la terminal o abrir una nueva sesión de Zsh para aplicar los cambios
 echo 'Ahora debes configurar el tema ingresando "p10k configure" en tu terminal \n(recuerda tener instalada la fuente tipográfica necesaria)' | lolcat -a -s 3000 
@@ -26,3 +30,6 @@ echo '"p10k configure"' |  lolcat -a -s 3000
 echo '"p10k configure"' |  lolcat -a -s 3000
 sleep 9
 reset
+
+# Carga la configuración de Zsh sin iniciar una nueva sesión
+source ~/.zshrc
